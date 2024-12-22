8904ee7e13e154190189f6ebdd7d19cf3742fd33
allan.yen <iamallan0917@gmail.com>
2024-12-22 20:26:09 +0800

    2

--- src/products.js
+++ src/products.js
@@ -3,7 +3,7 @@ export const products = [{
   'category': 'acoustic guitar',
   'content': '木吉他1',
   'description': '木吉他1',
-  'id': '1',
+  'id': '1',//window.crypto.randomUUID()
   'imageUrl': 'https://plus.unsplash.com/premium_photo-1681396937086-8a28edd8d257?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'imagesUrl': [
 	  'https://images.unsplash.com/photo-1520167112707-56e25f2d7d6e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
